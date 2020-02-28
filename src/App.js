4ffae30b214850eefe9e5d9814640b483a20a766
umairx97 <umair.bajwa@hotmail.com>
2020-02-29 03:26:55 +0500

    fix: Review code changes

--- src/App.js
+++ src/App.js
@@ -5,30 +5,26 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 library.add(faTrash);
+// USE ARROW FUNCTIONS AND THE STATE SYNTAX AS BELOW
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      currentItem: {
-        text: "",
-        key: ""
-      }
-    };
-    this.handleInput = this.handleInput.bind(this);
-    this.addItem = this.addItem.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
-    this.setUpdate = this.setUpdate.bind(this);
-  }
-  handleInput(e) {
+  state = {
+    items: [],
+    currentItem: {
+      text: "",
+      key: ""
+    }
+  };
+
+  handleInput = e => {
     this.setState({
       currentItem: {
         text: e.target.value,
         key: Date.now()
       }
     });
-  }
-  addItem(e) {
+  };
+  
+  addItem = e => {
     e.preventDefault();
     const newItem = this.state.currentItem;
     if (newItem.text !== "") {
@@ -41,25 +37,28 @@ class App extends React.Component {
         }
       });
     }
-  }
-  deleteItem(key) {
-    const filteredItem = this.state.items.filter(item => item.key !== key);
-    this.setState({
-      items: filteredItem
-    });
-  }
-  setUpdate(text, key) {
-    const items = this.state.items;
-    items.map(item => {
+  };
+  deleteItem = key => {
+    // Use destructuring whenver possible
+    const { items } = this.state;
+    const filteredItem = items.filter(item => item.key !== key);
+    this.setState({ items: filteredItem });
+  };
+
+  // Good work on updating the todo's 
+  setUpdate = (text, key) => {
+    // use destructuring + key assignment
+    const { items: updatedItems } = this.state
+    updatedItems.map(item => {
       if (item.key === key) {
         item.text = text;
       }
     });
-    this.setState({
-      items: items
-    });
-  }
+    this.setState({ items: updatedItems });
+  };
+
   render() {
+    const { items, currentItem } = this.state;
     return (
       <div className="App" onSubmit={this.addItem}>
         <header>
@@ -67,14 +66,14 @@ class App extends React.Component {
             <input
               type="tetx"
               placeholder="Enter Text"
-              value={this.state.currentItem.text}
+              value={currentItem.text}
               onChange={this.handleInput}
             />
             <button type="submit">Add</button>
           </form>
         </header>
         <ListItems
-          items={this.state.items}
+          items={items}
           deleteItem={this.deleteItem}
           setUpdate={this.setUpdate}
         />
