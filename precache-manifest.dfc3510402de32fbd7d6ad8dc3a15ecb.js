8a1674f765f4fa3cd0262106030b2f71a8374a48
sharozraees <sharozraees802@gmail.com>
2020-02-28 19:27:05 +0500

    Updates

---
+++ precache-manifest.dfc3510402de32fbd7d6ad8dc3a15ecb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9313b55e143f17fa191e91c7bb965936",
+    "url": "/React-TodoApp/index.html"
+  },
+  {
+    "revision": "ee4c6381110cc40607af",
+    "url": "/React-TodoApp/static/css/main.69dbdf87.chunk.css"
+  },
+  {
+    "revision": "8a5511781f9b92818c4d",
+    "url": "/React-TodoApp/static/js/2.e070290f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-TodoApp/static/js/2.e070290f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee4c6381110cc40607af",
+    "url": "/React-TodoApp/static/js/main.8b746926.chunk.js"
+  },
+  {
+    "revision": "030ed2f7d38b233a2cc9",
+    "url": "/React-TodoApp/static/js/runtime-main.68eacfa5.js"
+  }
+]);
